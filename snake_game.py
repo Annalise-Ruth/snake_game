61b7c1b442bb9a104d9b3a75d5da7f0cfeb04187
Annie <annaliseruth.delacruz@gmail.com>
2024-10-02 00:07:14 +0800

    Class Food and Class Snake are both working

--- snake_game.py
+++ snake_game.py
@@ -1,4 +1,4 @@
-from tkinter import *
+from tkinter import * 
 import random
 
 #Constants/Game Settings
@@ -13,10 +13,33 @@ background_color = "#99DFEC"
 
 #Classes Snake and Food
 class Snake:
-    pass
+    
+    def __init__(self):
+        self.body_size = body_parts
+        self.coordinates = []
+        self.squares = []
+
+        for i in range(0, body_parts):
+            self.coordinates.append([0, 0])
+
+        for x_axis, y_axis in self.coordinates:
+            square = game_canvas.create_rectangle(x_axis, y_axis, x_axis + space_size, y_axis + space_size, fill=snake_color, tags="snake")
+            self.squares.append(square)
+
 
 class Food:
-    pass
+
+    def __init__(self):
+     x_axis = random.randint(0, (game_width/space_size)-1) * space_size
+     y_axis = random.randint(0, (game_height/space_size)-1) * space_size 
+
+     self.coordinates = [x_axis, y_axis]
+
+     game_canvas.create_oval(x_axis, y_axis, x_axis + space_size, y_axis + space_size, fill=food_color, tag="food")
+    
+
+
+
 
 def next_turn():
     pass
@@ -43,6 +66,21 @@ score_label.pack()
 
 game_canvas = Canvas(window, bg=background_color, height=game_height, width=game_width)
 game_canvas.pack()
+
+window.update()
+
+window_width = window.winfo_width()
+window_height = window.winfo_height()
+screen_width = window.winfo_screenwidth()
+screen_height = window.winfo_screenheight()
+
+x_axis = int((screen_width/2) - (window_width/2))
+y_axis = int((screen_height/2) - (window_height/2))
+
+window.geometry(f"{window_width}x{window_height}+{x_axis}+{y_axis}")
+
+snake = Snake()
+food = Food()
 
 
 window.mainloop ()
