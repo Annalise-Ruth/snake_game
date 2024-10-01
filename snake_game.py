9f0a5b3bb3bf6d3ff94cdaa816e980b2a63b0e31
Annie <annaliseruth.delacruz@gmail.com>
2024-10-01 16:01:15 +0800

    Game Settings Done

---
+++ snake_game.py
@@ -0,0 +1,31 @@
+from _tkinter import *
+import random
+
+#Constants/Game Settings
+game_width = 700
+game_height = 700
+speed = 100
+space_size = 50
+body_parts = 3
+snake_color = "#9FDDA4"
+food_color = "#E0889F"
+background_color = "#99DFEC"
+
+#Classes Snake and Food
+class Snake:
+    pass
+
+class Food:
+    pass
+
+def next_turn():
+    pass
+
+def change_direction(new_direction):
+    pass
+
+def check_collisions():
+    pass
+
+def game_over():
+    pass
